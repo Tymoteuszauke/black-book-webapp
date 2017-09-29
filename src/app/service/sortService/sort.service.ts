e9751999c9c30caca4928144e3e666fbf37d5248
Mateusz Slabonski <mateusz_slabonski@epam.com>
2017-09-29 14:43:57 +0200

    free books are now more visible

--- src/app/service/sortService/sort.service.ts
+++ src/app/service/sortService/sort.service.ts
@@ -6,7 +6,7 @@ export class SortService {
   columns: string[];
 
   constructor() {
-    this.columns = ['book.title', 'book.authors', 'price','bookstore.name'];
+    this.columns = ['book.title,ASC', 'book.title,DESC', 'book.authors,ASC', 'book.authors,DESC', 'price,ASC', 'price,DESC', 'bookstore.name,ASC', 'bookstore.name,DESC'];
   }
 
 }
